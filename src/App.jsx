4b204cc7eaf7acb9456a441a15a0a28ef8955492
kaanmetep <kaanpmete@gmail.com>
2024-03-13 21:17:25 +0300

    feat:added selected movie state

--- src/App.jsx
+++ src/App.jsx
@@ -9,42 +9,54 @@ const KEY = "2dddb35f";
 const App = () => {
   const [error, setError] = useState(false);
   const [loader, setLoader] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
   const [totalResults, setTotalResults] = useState(0);
   const [input, setInput] = useState("");
   const [movies, setMovies] = useState(null);
   useEffect(
     function () {
-      (async () => {
+      const controller = new AbortController();
+      const getData = async () => {
         try {
-          if (input.length < 1 || !input) {
-            setMovies(null);
-            setError(false);
-            setLoader(false);
-            setTotalResults(0);
-            return;
-          }
           setError(false);
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${input}`
-          );
           setLoader(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${input}`,
+            { signal: controller.signal }
+          );
           if (!res.ok) throw new Error("An error occured.");
           const data = await res.json();
           if (data.Response === "False") throw new Error("film not found");
           setMovies(data.Search);
-          setLoader(false);
           setTotalResults(data.Search.length);
-          console.log(data.Search);
         } catch (err) {
-          setError(true);
-          setTotalResults(0);
+          if (err.name !== "AbortError") {
+            setError(true);
+            setTotalResults(0);
+          }
+        } finally {
+          setLoader(false);
         }
-      })();
+      };
+      if (input.length < 1 || !input) {
+        setMovies(null);
+        setError(false);
+        setLoader(false);
+        setTotalResults(0);
+        return;
+      }
+      getData();
+      return function () {
+        controller.abort();
+      };
     },
     [input]
   );
   const handleInput = (e) => {
     setInput(e.target.value);
+  };
+  const handleId = (id) => {
+    setSelectedId(id);
   };
   return (
     <div className="w-5/6 max-w-4xl bg-stone-200 mx-auto mt-10  rounded-lg pt-2">
@@ -53,8 +65,14 @@ const App = () => {
         <TotalResults totalResults={totalResults} />
       </Header>
       <MainBox>
-        <Box type="left" error={error} loader={loader} movies={movies} />
-        <Box type="right" />
+        <Box
+          type="left"
+          error={error}
+          loader={loader}
+          movies={movies}
+          onSelectId={handleId}
+        />
+        <Box type="right" selectedId={selectedId} />
       </MainBox>
     </div>
   );
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,8 @@
+const KEY = "2dddb35f";
+import { useEffect, useState } from "react";
 import Error from "./Error";
 import Loader from "./Loader";
-const Box = ({ type, movies, error, loader }) => {
+const Box = ({ type, movies, error, loader, onSelectId, selectedId }) => {
   if (type === "left")
     return (
       <div className="flex-1 border-r-2 border-stone-500 p-4 overflow-auto">
@@ -9,17 +11,24 @@ const Box = ({ type, movies, error, loader }) => {
         ) : loader === true ? (
           <Loader />
         ) : (
-          <MovieList movies={movies} />
+          <MovieList movies={movies} onSelectId={onSelectId} />
         )}
       </div>
     );
 
-  return <div className="flex-1  p-4">right</div>;
+  return (
+    <div className="flex-1 p-4 overflow-auto">
+      <MovieDetails selectedId={selectedId} />
+    </div>
+  );
 };
 
-const Movie = ({ movieObj }) => {
+const Movie = ({ movieObj, onSelectId }) => {
   return (
-    <li className="bg-stone-300 rounded-lg p-2 cursor-pointer flex gap-2 hover:bg-red-50 transition-all duration-150">
+    <li
+      className="bg-stone-300 rounded-lg p-2 cursor-pointer flex gap-2 hover:bg-red-50 transition-all duration-150"
+      onClick={() => onSelectId(movieObj.imdbID)}
+    >
       <img src={movieObj.Poster} alt="movie-poster" className="h-10" />
       <div className="">
         <p className="text-sm md:text-lg font-medium md:font-normal">
@@ -31,13 +40,61 @@ const Movie = ({ movieObj }) => {
   );
 };
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, onSelectId }) => {
   return (
     <ul className="flex flex-col gap-4 ">
       {movies?.map((movie) => (
-        <Movie movieObj={movie} key={movie.imdbID} />
+        <Movie movieObj={movie} key={movie.imdbID} onSelectId={onSelectId} />
       ))}
     </ul>
+  );
+};
+
+const MovieDetails = ({ selectedId }) => {
+  const [loader, setLoader] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState({});
+  useEffect(
+    function () {
+      (async () => {
+        try {
+          setLoader(true);
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          );
+          if (!res.ok) throw new Error("data could not fetched");
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("incorrect id");
+          setSelectedMovie(data);
+        } catch (error) {
+          console.log(error.message);
+        } finally {
+          setLoader(false);
+        }
+      })();
+    },
+    [selectedId]
+  );
+  return (
+    selectedId && (
+      <div>
+        <div className="flex gap-2 mb-4">
+          <img
+            src={selectedMovie.Poster}
+            alt="selectedMoviePoster"
+            className="w-14 sm:w-20 h-28"
+          />
+          <div className="flex flex-col sm:gap-2 sm:text-xs grow-0 text-[8px]">
+            <p className="font-bold mb-1 sm:mb-2 text-sm sm:text-lg">
+              {selectedMovie.Title}
+            </p>
+            <p>{selectedMovie.Genre}</p>
+            <p>{selectedMovie.Released}</p>
+            <p>{selectedMovie.Language}</p>
+          </div>
+        </div>
+        <p>{selectedMovie.Plot}</p>
+      </div>
+    )
   );
 };
 
