f9a7d74cdbb6142981c48a128ecdade9252994c5
kaanmetep <kaanpmete@gmail.com>
2024-03-17 03:14:44 +0300

    feat:auto focusing searchbox

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useRef } from "react";
+
 const SearchBox = ({ onSetInput, input }) => {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    const callback = (e) => {
+      inputEl.current.focus();
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, []);
+
   return (
     <input
       type="text"
       placeholder="Start searching..."
-      className="rounded-lg placholder:text-stone-200 px-4 py-1 text-xs w-40 sm:w-56 sm:focus:w-64 transition-all duration-300 focus:outline-none focus:ring focus:ring-red-100 focus:ring-opacity-50"
+      className="search rounded-lg placholder:text-stone-200 px-4 py-1 text-xs w-40 sm:w-56 sm:focus:w-64 transition-all duration-300 focus:outline-none focus:ring focus:ring-red-100 focus:ring-opacity-50"
       onChange={(e) => onSetInput(e)}
       value={input}
+      ref={inputEl}
     />
   );
 };
